7146d03cb15a3a0377bfae32e06e69eb54ad1983
leobrda <leonardo-breda1@hotmail.com>
2026-05-02 15:03:47 -0300

    Implementa formulário e view para criação de coleções pelo usuário

--- albums/urls.py
+++ albums/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.album_list, name='album_list'),
+    path('new/', views.collection_create, name='collection_create'),
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Collection
+from .forms import CollectionForm
+
 
 @login_required
 def album_list(request):
     my_collections = Collection.objects.filter(owner=request.user)
     return render(request, 'albums/album_list.html', {'collections': my_collections})
+
+
+@login_required
+def collection_create(request):
+    if request.method == 'POST':
+        form = CollectionForm(request.POST)
+        if form.is_valid():
+            collection = form.save(commit=False)
+            collection.owner = request.user
+            collection.save()
+            return redirect('album_list')
+    else:
+        form = CollectionForm()
+
+    return render(request, 'albums/collection_form.html', {'form': form})
