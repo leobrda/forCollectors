7fb95881ec667dc452dcf90ba584c68e4208d22b
leobrda <leonardo-breda1@hotmail.com>
2026-05-04 11:14:53 -0300

    Implementa upload de itens com Cloudinary e página de detalhes da coleção

--- albums/urls.py
+++ albums/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.album_list, name='album_list'),
     path('new/', views.collection_create, name='collection_create'),
+    path('collection/<int:pk>/', views.collection_detail, name='collection_detail'),
+    path('collection/<int:collection_pk>/add-item/', views.item_create, name='item_create'),
 ]
